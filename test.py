4efb8f93ff9c6cef4c36cd6c552fc072a7220385
joelkbiju12 <joel.biju@headout.com>
2023-04-05 23:51:39 +0530

    Adding auth

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ data = {"ref":"main","inputs":{"environment":"12345678"}}
 #response = requests.post(url,json=data,headers=headers)
 #print(response)
 print(datetime.now())
-redis = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True)
+redis = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True,password='5%p2*8uLjm&i')
 print(redis.get('test'))
 data = {"text":"Test1"}
 response = requests.post(url,json=data)
