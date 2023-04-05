7d4595ba1dbb4fbb7bec03d457a9caddfea768d9
Joel K Biju <joelkbiju18@gmail.com>
2023-04-05 23:09:52 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import requests
+import subprocess
+import redis
+from datetime import datetime, timedelta, time
+
+github_token = os.environ["CI_TOKEN"]
+url = os.environ["URL"]
+headers = {"Authorization": "token %s" % github_token}
+data = {"ref":"main","inputs":{"environment":"12345678"}}
+response = requests.post(url,json=data,headers=headers)
+print(response)
+print(datetime.now())
+redis = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True)
+print(redis.get('test'))
+data = {"text":"Test1"}
+response = requests.post(os.environ["URL"],json=data)
