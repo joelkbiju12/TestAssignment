d376591dacfd666284757f6bca865556ca885bc1
Joel K Biju <joelkbiju18@gmail.com>
2023-04-05 23:23:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ import redis
 import os
 from datetime import datetime, timedelta, time
 
-github_token = os.environ["CI_TOKEN"]
+#github_token = os.environ["CI_TOKEN"]
 url = os.environ["URL"]
-headers = {"Authorization": "token %s" % github_token}
+#headers = {"Authorization": "token %s" % github_token}
 data = {"ref":"main","inputs":{"environment":"12345678"}}
-response = requests.post(url,json=data,headers=headers)
+#response = requests.post(url,json=data,headers=headers)
 print(response)
 print(datetime.now())
 redis = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True)
