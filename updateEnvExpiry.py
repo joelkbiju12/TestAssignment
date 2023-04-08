a44c61463ba770c00ddf068f2a015b5f296a18ca
Joel K Biju <joelkbiju18@gmail.com>
2023-04-08 11:09:23 +0530

    Adding sleep

--- updateEnvExpiry.py
+++ updateEnvExpiry.py
@@ -2,14 +2,17 @@ import redis
 import os
 from datetime import datetime, timedelta, time
 from pytz import timezone
+import time
+
 def update_expiry():
+    time.sleep(30)
     try:
         redis_password = os.environ["EXPIRY_REDIS_PASSWORD"]
         ondemand_env = os.environ["DEPLOY_NAMESPACE"]
-        redisClient = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True,password=redis_password)
+        #redisClient = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True,password=redis_password)
         new_expiry = str(datetime.now(timezone("Asia/Kolkata"))+timedelta(days=3))
-        redisClient.set(ondemand_env,new_expiry)
-        msg = redisClient.get(ondemand_env)
+        #redisClient.set(ondemand_env,new_expiry)
+        #msg = redisClient.get(ondemand_env)
         print(f"Namepace: {ondemand_env}\nExpiry: {new_expiry}")
         print("Successfully updated expiry date")
     except Exception as e:
