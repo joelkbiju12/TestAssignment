d93c86b54e73629152f2f153de0ca80134b72c85
Joel K Biju <joelkbiju18@gmail.com>
2023-04-11 23:38:56 +0530

    Update updateEnvExpiry.py

--- updateEnvExpiry.py
+++ updateEnvExpiry.py
@@ -3,29 +3,20 @@ import os
 import sys
 from datetime import datetime, timedelta, time
 from pytz import timezone
-import time
 
+#Updating or removing redis key value for expiry of ondemand environment
 def update_expiry():
-    print(sys.argv[1])
+    redis_password = os.environ["EXPIRY_REDIS_PASSWORD"]
+    ondemand_env = os.environ["DEPLOY_NAMESPACE"]
+    redisClient = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True,password=redis_password)
+    new_expiry = str(datetime.now(timezone("Asia/Kolkata"))+timedelta(days=3))
     if sys.argv[1] == "update":
-        print("Updating redis key")
-    if sys.argv[1] == "delete":
-        print("Deleting redis key")
-    try:
-        redis_password = os.environ["EXPIRY_REDIS_PASSWORD"]
-        print(redis_password)
-        ondemand_env = os.environ["DEPLOY_NAMESPACE"]
-        redisClient = redis.StrictRedis(host='platform.redis.test-headout.com', port=6379,ssl=True,password=redis_password)
-        new_expiry = str(datetime.now(timezone("Asia/Kolkata"))+timedelta(days=3))
         redisClient.set(ondemand_env,new_expiry)
-        msg = redisClient.get(ondemand_env)
-        print(msg)
         print(f"Namepace: {ondemand_env}\nExpiry: {new_expiry}")
         print("Successfully updated expiry date")
-    except Exception as e:
-        print("Expiry date update failed")
-        print(e)
-
+    elif sys.argv[1] == "delete":
+        redisClient.delete(ondemand_env)
+        print("Deleted expiry date key")
 
 if __name__ == '__main__':
     update_expiry()
